d73d8fca7961abd3163c6dab63f9672a473e8fbe
harshalkale0911 <hkale6888@gmail.com>
2024-06-13 19:37:32 +0530

    service page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,20 @@
+import { Route, Routes } from 'react-router'
 import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import ServicePage from './components/Services'
 
 function App() {
 
 
   return (
      <>
-     <Navbar />
-     <Home />
+   <Routes>
+ 
+<Route path="/services"  element={<ServicePage />} />
+
+</Routes>
+     
      </>
   )
 }
