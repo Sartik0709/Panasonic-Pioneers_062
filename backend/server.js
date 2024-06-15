e168cae0fad17be26fac5cb9d86eb0cf6c79d65f
RAKESH GOWDA <rakeshgowda495758@gmail,com>
2024-06-15 19:32:33 +0530

    backend update from rakesh_8

--- backend/server.js
+++ backend/server.js
@@ -1,61 +1,46 @@
-import express from 'express'
-import {config} from 'dotenv'
+import express from 'express';
+import { config } from 'dotenv';
 import { connecttodb } from './src/config/db.js';
 import { pet } from './src/routes/petRoute.js';
 import { userRoute } from './src/routes/userRoute.js';
-import cors from 'cors'
+import cors from 'cors';
 import { servicePRovider } from './src/routes/serviceProvider.js';
 import { auth } from './src/middlewares/auth.js';
-
-
+import path from 'path';
+import fs from 'fs';
 
 config();
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 
+// Ensure the 'uploads' directory exists
 const uploadsDir = path.join(process.cwd(), 'uploads');
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir, { recursive: true });
 }
 
+// Serve static files from the 'uploads' directory
 app.use('/uploads', express.static(uploadsDir));
 
-app.use(cors())
+app.use(cors());
 
-const port=process.env.PORT||9090;
+const port = process.env.PORT || 9090;
+const uri = process.env.URI || null;
 
-const uri=process.env.URI||null;
+app.use("/user", userRoute);
+app.use("", pet);
+app.use("", servicePRovider);
 
-// app.use(session({
-//     secret:process.env.JWT_SEACRET,
-//     Store:MongoStore.create({
-//         mongoUrl:uri,
-//         collectionName:'session'
-//     }),
-//     resolve:false,
-//     saveUninitialized:false,
-//     Cookie:{
-//         maxAge:1000*60*60
-//     }
-// }))
-
-app.use("/user",userRoute);
-
-app.use("",pet)
-
-app.use("",servicePRovider)
-
-app.listen(port,async()=>{
-    try{
-        await connecttodb(uri);
-        console.log('database connected successfully')
-        console.log(`server connected at the port number ${port}`);
-    }catch(err){
-        console.log(err);
-    }
-    
-})
+app.listen(port, async () => {
+  try {
+    await connecttodb(uri);
+    console.log('database connected successfully');
+    console.log(`server connected at the port number ${port}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 export default app;
