0782c011819de961ecdcfae6d287a05b5e432d06
RAKESH GOWDA <rakeshgowda495758@gmail,com>
2024-06-17 12:20:58 +0530

    backend from rakesh 11

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -16,6 +16,7 @@ import ServicePayment from './pages/ServicePaymentPage';
 import About from './pages/About';
 import Register from './components/Register';
 import { AdminPage } from './pages/AdminPage';
+import Petform from './components/Petform';
 
 
 
@@ -23,38 +24,38 @@ function App() {
   return (
 
 
-    //  <>
-    // <Petform/>
-    //  </>
+     <>
+    <Petform/>
+     </>
     // <>
     //  <ForgotPassword />
     // </>
 
-    <>
-      <Stack spacing={20}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/adminPage" element={<AdminPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/Register" element={<Register/>} />
-          <Route path="/services"  element={<ServicePage />}/>
-          <Route path="/servicePaymentPage" element={<ServicePayment />} />
-          <Route path="/booking/:serviceId" element={<BookingPage />} />
-          <Route path="/login" element={<Login />} />
+    // <>
+    //   <Stack spacing={20}>
+    //     <Navbar />
+    //     <Routes>
+    //       <Route path="/" element={<HomePage />} />
+    //       <Route path="/adminPage" element={<AdminPage />} />
+    //       <Route path="/home" element={<HomePage />} />
+    //       <Route path="/Register" element={<Register/>} />
+    //       <Route path="/services"  element={<ServicePage />}/>
+    //       <Route path="/servicePaymentPage" element={<ServicePayment />} />
+    //       <Route path="/booking/:serviceId" element={<BookingPage />} />
+    //       <Route path="/login" element={<Login />} />
 
-          <Route path="/Adoptpage" element={<Adoptpetpage />} />
-          <Route path="/payment/:petId" element={<PaymentPage />} />
-          <Route path="/forgot" element={<ForgotPassword/>}/>
-          <Route path="/reset" element={<PasswordReset/>} />
+    //       <Route path="/Adoptpage" element={<Adoptpetpage />} />
+    //       <Route path="/payment/:petId" element={<PaymentPage />} />
+    //       <Route path="/forgot" element={<ForgotPassword/>}/>
+    //       <Route path="/reset" element={<PasswordReset/>} />
 
-          <Route path="/Adoptpage" element={<Adoptpetpage />} />  
-          <Route path="/about" element={<About />} />
+    //       <Route path="/Adoptpage" element={<Adoptpetpage />} />  
+    //       <Route path="/about" element={<About />} />
 
-        </Routes>
+    //     </Routes>
         
-      </Stack>
-    </>
+    //   </Stack>
+    // </>
   );
 
 }
